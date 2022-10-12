abc4b6dbc5eccee9c2bef6cea33e9eb896268e84
Andrew Burgess <andrew@burgess.io>
2022-10-12 09:50:45 -0400

    error handling cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,67 @@
+use std::env;
 use std::io::{Read, Write};
 use std::net::{Shutdown, TcpListener, TcpStream};
 use std::thread;
 
-fn main() {
-    println!("Hello, TCP!");
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+const MAX_MESSAGE_SIZE: usize = 128;
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                thread::spawn(|| handle_connection(stream));
+fn main() {
+    let default_port: Result<String, &str> = Ok("8080".to_string());
+
+    match env::var("RUDEST_PORT").or(default_port) {
+        Ok(port_string) => match port_string.parse::<usize>() {
+            Ok(port) => {
+                let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).unwrap();
+                println!("listening on port {}", port);
+
+                for stream in listener.incoming() {
+                    match stream {
+                        Ok(stream) => {
+                            thread::spawn(|| handle_connection(stream));
+                        }
+                        Err(err) => println!("error opening stream: {}", err),
+                    }
+                }
             }
-            Err(err) => println!("stream error: {}", err),
-        }
+            Err(err) => println!("error parsing port: {}", err),
+        },
+        Err(err) => println!("error getting port: {}", err),
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buf = [0; 128];
-    println!("stream: {stream:?}");
+    let mut buf = [0; MAX_MESSAGE_SIZE];
+
     loop {
         match stream.read(&mut buf) {
             Ok(len) => {
                 if len == 0 {
-                    println!("goodbye!");
                     break;
                 }
 
-                let msg = std::str::from_utf8(&buf[0..len]).unwrap().trim();
-                println!("message: {}", msg);
+                match std::str::from_utf8(&buf[0..len]) {
+                    Ok(msg) => {
+                        let trimmed_msg = msg.trim();
+                        println!("received: {}", trimmed_msg);
 
-                match stream.write(&buf[0..len]) {
-                    Ok(len) => {
-                        println!("sent length {}", len);
-                    }
-                    Err(err) => {
-                        println!("error when writing: {}", err);
-                    }
-                }
+                        match stream.write(&buf[0..len]) {
+                            Ok(len) => {
+                                println!("sent length {}", len);
+                            }
+                            Err(err) => println!("error writing: {}", err),
+                        }
 
-                if msg == "quit" {
-                    match stream.shutdown(Shutdown::Both) {
-                        Ok(_) => break,
-                        Err(_) => break,
+                        if trimmed_msg == "quit" {
+                            match stream.shutdown(Shutdown::Both) {
+                                Ok(_) => break,
+                                Err(err) => println!("error shutting down stream: {}", err),
+                            }
+                        }
                     }
+                    Err(err) => println!("error receiving message: {}", err),
                 }
             }
-            Err(err) => {
-                println!("error when reading: {}", err);
-            }
+            Err(err) => println!("error reading: {}", err),
         }
     }
 }
